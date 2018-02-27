1cdfbe1731efe2be75e60d5844b146094eca1104
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-28 00:54:56 +0300

    - Date extension callender date type added

--- RoomBooking/Utility/Extensions/DateExtension.swift
+++ RoomBooking/Utility/Extensions/DateExtension.swift
@@ -13,4 +13,13 @@ extension Date {
         let timestamp = self.timeIntervalSince1970
         return String(describing: timestamp)
     }
+    
+    var callender:String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "en_GB")
+        dateFormatter.setLocalizedDateFormatFromTemplate("dd MMMM yyyy")
+        return dateFormatter.string(from: self)
+    }
 }

4833f3d094b33e96a41167e74826f6055b477363
Dagdelen, Alisan(AWF) <adagdelen@ebay.com>
2018-03-02 04:47:30 +0300

    - BeginTime and EndTime added to init method - Date create logic implemented

--- RoomBooking/ViewModels/BookRoomViewModel.swift
+++ RoomBooking/ViewModels/BookRoomViewModel.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 enum InvalidFieldType:String {
-    case name           = "Name invalid"
-    case email          = "Email invalid"
-    case phoneNo        = "Phone Number invalid"
-    case title          = "Title invalid"
-    case descpription   = "Description invalid"
-    case emptyAttendees = "Attendees empty"
-    case existAttendee  = "Attendee already exist"
+    case name             = "Name invalid"
+    case email            = "Email invalid"
+    case phoneNo          = "Phone Number invalid"
+    case title            = "Title invalid"
+    case descpription     = "Description invalid"
+    case emptyAttendees   = "Attendees empty"
+    case existAttendee    = "Attendee already exist"
+    case emptyBookingTime = "Booking time empty"
 }
 
 protocol BookRoomViewModelProtocol {
@@ -25,6 +26,7 @@ protocol BookRoomViewModelProtocol {
     var roomName: String { get }
     var availableHours:[String] { get }
     func addAttendee(name:String, email:String, phoneNo:String)
+    func selectBookingTime(timeStart:String, timeEnd:String)
     func deleteAttendee(_ attendee:Attendee)
     func bookRoom(title:String, description:String, _ completion: @escaping SuccessBlock)
 }
@@ -46,7 +48,9 @@ class BookRoomViewModel: NSObject, BookRoomViewModelProtocol {
     private var room:Room!
     private var date:Date!
     private var dataService:RoomBookingApi
-
+    private var beginTime:String!
+    private var endTime:String!
+    
     init(room:Room, date:Date, dataService:RoomBookingApi = RoomBookingService.sharedInstance) {
         self.room = room
         self.date = date
@@ -54,6 +58,8 @@ class BookRoomViewModel: NSObject, BookRoomViewModelProtocol {
         self.attendees = Dynamic([])
         self.invalidAttendeeFields = Dynamic([])
         self.invalidBookingFields = Dynamic([])
+        self.beginTime = ""
+        self.endTime = ""
         super.init()
     }
     
@@ -71,9 +77,20 @@ class BookRoomViewModel: NSObject, BookRoomViewModelProtocol {
         }
     }
     
+    func selectBookingTime(timeStart:String, timeEnd:String) {
+        let startHour = timeStart.hourIntTuple.hour
+        let startMinute = timeStart.hourIntTuple.minute
+        let endHour = timeEnd.hourIntTuple.hour
+        let endMinute = timeEnd.hourIntTuple.minute
+        guard let beginDateUnix = formatBookingDate(hour: startHour, minute: startMinute) else { return }
+        guard let endDateUnix = formatBookingDate(hour: endHour, minute: endMinute) else { return }
+        beginTime = beginDateUnix
+        endTime = endDateUnix
+    }
+    
     func bookRoom(title: String, description: String, _ completion: @escaping SuccessBlock) {
         if validateBookingFields(title: title, description: description) {
-            let booking = Booking(date: date.unixDate, timeStart: "1519905600", timeEnd: "1519912800", title: title, description: description, roomName: room.name)
+            let booking = Booking(date: date.unixDate, timeStart: beginTime, timeEnd: endTime, title: title, description: description, roomName: room.name)
             let request = BookRoomRequest(booking: booking, attendees: attendees.value)
             dataService.postBooking(request, { (response, error) in
                 if let response = response {
@@ -83,6 +100,16 @@ class BookRoomViewModel: NSObject, BookRoomViewModelProtocol {
                 }
             })
         }
+    }
+    
+    private func formatBookingDate(hour:Int, minute:Int) -> String? {
+        var dateComponents = DateComponents()
+        dateComponents.year = Calendar.current.component(.year, from: date)
+        dateComponents.month = Calendar.current.component(.month, from: date)
+        dateComponents.day = Calendar.current.component(.day, from: date)
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        return Calendar.current.date(from: dateComponents)?.unixDate
     }
     
     private func validateAttendeeFields(name:String, email:String, phoneNo:String) -> Bool {
@@ -116,6 +143,9 @@ class BookRoomViewModel: NSObject, BookRoomViewModelProtocol {
         }
         if (attendees.value.count < 1) {
             invalidFields.append(.emptyAttendees)
+        }
+        if (endTime.count < 1 && beginTime.count < 1) {
+            invalidFields.append(.emptyBookingTime)
         }
         invalidBookingFields.value = invalidFields
         let result = invalidBookingFields.value.count == 0 ? true : false
